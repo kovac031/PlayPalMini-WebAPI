267d95c53464eb8e1fec3eec02cd19f3f17b9a17
kovac031 <ivan.kovac.email@gmail.com>
2023-08-15 13:37:55 +0200

    authentication good progress

---
+++ PlayPalMini/PlayPalMini.WebAPI/Filters/JwtAuthenticationAttribute.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Web;
+using System.Web.Http.Filters;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace PlayPalMini.WebAPI.Filters
+{
+    public class JwtAuthenticationAttribute : Attribute, IAuthenticationFilter
+    {
+        public async Task AuthenticateAsync(HttpAuthenticationContext context, CancellationToken cancellationToken)
+        {
+            HttpRequestMessage request = context.Request;
+            AuthenticationHeaderValue authorization = request.Headers.Authorization;
+
+            if (authorization == null || authorization.Scheme != "Bearer" || string.IsNullOrEmpty(authorization.Parameter))
+            {
+                context.ErrorResult = new AuthenticationFailureResult("Missing JWT Token", request);
+                return;
+            }
+
+            string jwtToken = authorization.Parameter;
+            if (!IsValidToken(jwtToken))
+            {
+                context.ErrorResult = new AuthenticationFailureResult("Invalid JWT Token", request);
+            }
+        }
+
+        public async Task ChallengeAsync(HttpAuthenticationChallengeContext context, CancellationToken cancellationToken) { }
+
+        public bool AllowMultiple => false;
+
+        private bool IsValidToken(string token)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes("54321");  // same key as used during token creation
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = "PlayPalMini",
+                ValidAudience = "PlayPalMini",
+                IssuerSigningKey = new SymmetricSecurityKey(key)
+            };
+
+            try
+            {
+                tokenHandler.ValidateToken(token, tokenValidationParameters, out SecurityToken validatedToken);
+                return true;
+            }
+            catch
+            {
+                return false;  // token validation failed
+            }
+        }
+
+    }
+}
